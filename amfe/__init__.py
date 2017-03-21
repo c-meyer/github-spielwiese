cf55bf21ae92c63a7177bf1e6a07f5b80c215411
Christian Meyer <christian.meyer@tum.de>
2017-03-21 14:06:17 +0100

    Added testfiles

---
+++ amfe/__init__.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Module for advanced nonlinear finite element analysis. This software is
+developed and maintained at the Chair for Applied Mechanics,
+Technical University of Munich.
+
+Authors:
+Johannes Rutzmoser, Fabian Gruber
+"""
+from __future__ import absolute_import
+
+# Finite Element stuff
+from .material import *
+
+
+__author__ = 'Johannes Rutzmoser, Fabian Gruber'
